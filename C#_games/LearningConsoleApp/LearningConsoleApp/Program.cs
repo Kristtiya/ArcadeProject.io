615925799587c60e00ce6720e031890c5ae6edd5
Kristtiya <kristtiyaguerra3.14@gmail.com>
2024-05-04 15:12:18 -0400

    Create starter files for C# and C++

---
+++ C#_games/LearningConsoleApp/LearningConsoleApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+// Comment
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string name = "Kris";
+            int age = 23;
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("Age: " + age);
+        }
+    }
+}
+
+/* Data Types:
+int:     
+double:  
+char:    
+bool:    
+string: 
+ */
